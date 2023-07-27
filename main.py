085bd662bf39825c23669c9e7a2ee59772b12784
kwa_laa <khoaddnguyen@gmail.com>
2023-07-27 14:23:21 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Russian Roulette
+import random
+
+names_string = input("Give me everybody's names, separated by a comma. \n")
+names = names_string.split(", ")
+
+#random.randint(0,x), x = num_items - 1
+num_items = len(names)
+random_choice = random.randint(0, num_items - 1)
+
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the meal today.")
